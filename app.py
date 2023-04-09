562b8c5bd5e7d970140c65cbcc0da1649192a3a8
behnamsaba <behnams71@gmail.com>
2023-04-08 18:41:57 -0700

    fixed buggs

--- app.py
+++ app.py
@@ -14,244 +14,245 @@ app = Flask(__name__)
 app.app_context().push()
 
 
-uri = os.environ.get('DATABASE_URL', 'postgresql:///dictionary')
+uri = os.environ.get("DATABASE_URL", "postgresql:///dictionary")
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = uri
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY","helloworld1")
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "helloworld1")
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 connect_db(app)
 
 toolbar = DebugToolbarExtension(app)
 
 
-
 ##############################################################################
 # User signup/login/logout
 
 
-@app.route('/register',methods=["GET","POST"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
     if "user_id" in session:
-        return redirect ('/')
-    
+        return redirect("/")
+
     else:
-        form=UserForm()
+        form = UserForm()
         if form.validate_on_submit():
-            first_name=form.first_name.data
-            last_name=form.last_name.data
-            password=form.password.data
-            email=form.email.data
-            new_user= User.register(email,password,first_name,last_name)
+            first_name = form.first_name.data
+            last_name = form.last_name.data
+            password = form.password.data
+            email = form.email.data
+            new_user = User.register(email, password, first_name, last_name)
             db.session.add(new_user)
             try:
                 db.session.commit()
-                session['user_id']=new_user.id
+                session["user_id"] = new_user.id
             except IntegrityError:
-                form.email.errors.append('This email has already been added')
-                return render_template('register.html', form=form)
-            
-            
-            return redirect('/')
-        return render_template("register.html",form=form)
+                form.email.errors.append("This email has already been added")
+                return render_template("register.html", form=form)
+
+            return redirect("/")
+        return render_template("register.html", form=form)
 
 
-
-@app.route('/login', methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     """Handle user login."""
-    if 'user_id' in session:
-        return redirect('/')
+    if "user_id" in session:
+        return redirect("/")
     else:
         form = LoginForm()
         if form.validate_on_submit():
-            user = User.authenticate(form.email.data,
-                                    form.password.data)
+            user = User.authenticate(form.email.data, form.password.data)
 
             if user:
-                session['user_id']=user.id
+                session["user_id"] = user.id
                 # flash(f"Hello, {user.first_name}!", "success")
                 return redirect("/")
 
-            flash("Invalid credentials.", 'danger')
+            flash("Invalid credentials.", "danger")
 
-        return render_template('login.html', form=form)
+        return render_template("login.html", form=form)
 
-@app.route('/logout')
+
+@app.route("/logout")
 def logout_user():
-    if 'user_id' not in session:
-        return redirect('/')
+    if "user_id" not in session:
+        return redirect("/")
 
     session.pop("user_id")
     flash("Goodbye!", "info")
-    return redirect('/login')
+    return redirect("/login")
+
 
 @app.route("/<int:user_id>")
 def profile(user_id):
     if "user_id" not in session:
-        return redirect('/login')
+        return redirect("/login")
 
-    user=User.query.get_or_404(int(user_id))
-    words=[word.word_id for word in user.user_words]
+    user = User.query.get_or_404(int(user_id))
+    words = [word.word_id for word in user.user_words]
     all_words = [Word.query.get(x) for x in words]
-    return render_template('profile.html',user=user,all_words=all_words)
+    return render_template("profile.html", user=user, all_words=all_words)
+
 
 ##########################################################################################################
-#words routes (GENERAL USE, ADD,)
+# words routes (GENERAL USE, ADD,)
 
-@app.route('/')
+
+@app.route("/")
 def home_page():
-    return render_template('homepage.html')
+    return render_template("homepage.html")
 
 
-@app.route("/api/get-word",methods=["GET","POST"])
+@app.route("/api/get-word", methods=["GET", "POST"])
 def api_search():
-    form_word=SearchWord()
-    form_selection=AllSelection()
-    if session.get('user_id'):
+    form_word = SearchWord()
+    form_selection = AllSelection()
+    if session.get("user_id"):
         user = User.query.get_or_404(int(session["user_id"]))
-        form_selection.selection_list.choices=[(x.id,x.name) for x in user.user_selection]
-        form_selection.selection_list.choices.append(('New','New Category...'))
-        
-    
-
-        
+        form_selection.selection_list.choices = [
+            (x.id, x.name) for x in user.user_selection
+        ]
+        form_selection.selection_list.choices.append(("New", "New Category..."))
 
     if form_word.validate_on_submit():
-        user_input=form_word.search.data
+        user_input = form_word.search.data
         print(user_input)
-        api_request = requests.get("https://api.dictionaryapi.dev/api/v2/entries/en/"+user_input)
+        api_request = requests.get(
+            "https://api.dictionaryapi.dev/api/v2/entries/en/" + user_input
+        )
         result = api_request.json()
 
         try:
-            word=result[0]['word']
-            audio=""
-            
-            audio2=result[0]['phonetics']
+            word = result[0]["word"]
+            audio = ""
+
+            audio2 = result[0]["phonetics"]
             for x in audio2:
-                if x['audio'] != '':
-                    audio=x['audio']
+                if x["audio"] != "":
+                    audio = x["audio"]
 
-            if len(result[0]['phonetics']) != 0:
-                pronunciation=result[0]['phonetics'][0].get('text','')
+            if len(result[0]["phonetics"]) != 0:
+                pronunciation = result[0]["phonetics"][0].get("text", "")
             else:
-                pronunciation= ''
-            
-            
-            definition=result[0]['meanings'][0]['definitions'][0]['definition']
-            synonyms=result[0]['meanings'][0]['synonyms']
-            grammer=result[0]['meanings'][0]['partOfSpeech']
-            examples=result[0]['meanings'][0]['definitions'][0].get('example','')
-            answer={
-                "word":word,
-                "audio":audio,
-                "definition":definition,
-                "grammer":grammer,
-                "pronunciation":pronunciation,
-                "synonyms":synonyms,
-                "examples":examples,
+                pronunciation = ""
+
+            definition = result[0]["meanings"][0]["definitions"][0]["definition"]
+            synonyms = result[0]["meanings"][0]["synonyms"]
+            grammer = result[0]["meanings"][0]["partOfSpeech"]
+            examples = result[0]["meanings"][0]["definitions"][0].get("example", "")
+            answer = {
+                "word": word,
+                "audio": audio,
+                "definition": definition,
+                "grammer": grammer,
+                "pronunciation": pronunciation,
+                "synonyms": synonyms,
+                "examples": examples,
             }
-            
-            session['answer']=answer
-            
+
+            session["answer"] = answer
+
         except:
-            flash('Word Not Found!', 'danger')
-            return redirect('/api/get-word')
+            flash("Word Not Found!", "danger")
+            return redirect("/api/get-word")
 
-        return render_template('home.html',form=form_word,answer=answer,form_selection=form_selection)
+        return render_template(
+            "home.html", form=form_word, answer=answer, form_selection=form_selection
+        )
+
+    return render_template("home.html", form=form_word, form_selection=form_selection)
 
 
-    
-
-    return render_template('home.html',form=form_word,form_selection=form_selection)
-
-@app.route('/<int:user_id>/card/new',methods=["POST"])
+@app.route("/<int:user_id>/card/new", methods=["POST"])
 def add_word(user_id):
     if "user_id" not in session:
-        return redirect('/login')
+        return redirect("/login")
 
-    card_selection = request.form['selection_list']
-    if card_selection == 'New':
-        return redirect (f"/{session['user_id']}/categories/add")
+    card_selection = request.form["selection_list"]
+    if card_selection == "New":
+        return redirect(f"/{session['user_id']}/categories/add")
 
-    new_word = Word(word=session['answer']['word'], 
-    definition=session['answer']['definition'],
-    grammer=session['answer']['grammer'],
-    example=session['answer']['examples'],
-    audio=session['answer']['audio'],
-    pronunciation=session['answer']['pronunciation'],
-    synonyms=session['answer']['synonyms'],
-    selection_id=card_selection)
+    new_word = Word(
+        word=session["answer"]["word"],
+        definition=session["answer"]["definition"],
+        grammer=session["answer"]["grammer"],
+        example=session["answer"]["examples"],
+        audio=session["answer"]["audio"],
+        pronunciation=session["answer"]["pronunciation"],
+        synonyms=session["answer"]["synonyms"],
+        selection_id=card_selection,
+    )
     db.session.add(new_word)
     db.session.commit()
-    user_words =UserWord(user_id=user_id,word_id=new_word.id)
+    user_words = UserWord(user_id=user_id, word_id=new_word.id)
     db.session.add(user_words)
     db.session.commit()
     flash(f"{new_word.word} Added!", "success")
-    return redirect ('/')
+    return redirect("/")
 
-@app.route('/card/<int:word_id>/delete/',methods=["POST"])
+
+@app.route("/card/<int:word_id>/delete/", methods=["POST"])
 def delete_word(word_id):
     if "user_id" not in session:
-        return redirect('/login')
+        return redirect("/login")
 
-    word=Word.query.get_or_404(word_id)
+    word = Word.query.get_or_404(word_id)
     db.session.delete(word)
     flash(f"{word.word} Deleted!", "danger")
     db.session.commit()
     return redirect(f'/{session["user_id"]}/categories')
-    
-
-
-
 
 
 #########################################################################################################
-#category routes (ALL, ADD, DELETE)
+# category routes (ALL, ADD, DELETE)
+
 
 @app.route("/<int:user_id>/categories")
 def all_categories(user_id):
     """Show all cards"""
     if "user_id" not in session:
-        return redirect('/login')
+        return redirect("/login")
 
-    user=User.query.get_or_404(user_id)
-    return render_template ('selection.html',user=user)
+    user = User.query.get_or_404(user_id)
+    return render_template("selection.html", user=user)
 
-@app.route("/<int:user_id>/categories/add",methods=["GET","POST"])
+
+@app.route("/<int:user_id>/categories/add", methods=["GET", "POST"])
 def add_selection(user_id):
     if "user_id" not in session:
-        return redirect('/login')
+        return redirect("/login")
 
-    form=AddSelection()
+    form = AddSelection()
     if form.validate_on_submit():
-        name=form.name.data
-        new_selection=Selection(name=name,user_id=user_id)
+        name = form.name.data
+        new_selection = Selection(name=name, user_id=user_id)
         db.session.add(new_selection)
         db.session.commit()
-        flash(f'{new_selection.name} added', "success")
-        return redirect(f'/{user_id}/categories')
+        flash(f"{new_selection.name} added", "success")
+        return redirect(f"/{user_id}/categories")
 
-    return render_template('selectionform.html',form=form)
+    return render_template("selectionform.html", form=form)
+
 
 @app.route("/categories/<int:category_id>")
 def category_info(category_id):
     if "user_id" not in session:
-        return redirect('/login')
+        return redirect("/login")
 
-    category=Selection.query.get_or_404(category_id)
+    category = Selection.query.get_or_404(category_id)
 
-    return render_template ('category_info.html',category=category)
+    return render_template("category_info.html", category=category)
 
 
-@app.route("/categories/<int:category_id>/delete",methods=["POST"])
+@app.route("/categories/<int:category_id>/delete", methods=["POST"])
 def delete_selection(category_id):
     if "user_id" not in session:
-        return redirect('/login')
+        return redirect("/login")
 
     category = Selection.query.get_or_404(category_id)
     db.session.delete(category)
@@ -259,6 +260,5 @@ def delete_selection(category_id):
     # flash(f'{category.name} Deleted', "danger")
     return redirect(f'/{session["user_id"]}/categories')
 
-#################################################################################################################
 
 
--- forms.py
+++ forms.py
@@ -2,23 +2,36 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField, SelectField
 from wtforms.validators import InputRequired, Length, DataRequired
 
+
 class SearchWord(FlaskForm):
-    search = StringField("Enter Word", validators=[InputRequired()], render_kw={"placeholder": "Search English"})
+    search = StringField(
+        "Enter Word",
+        validators=[InputRequired()],
+        render_kw={"placeholder": "Search English"},
+    )
+
 
 class AddSelection(FlaskForm):
     name = StringField("Selection Name", validators=[InputRequired()])
 
+
 class UserForm(FlaskForm):
-    first_name=StringField("First Name",validators=[InputRequired(), Length(min= 2, max=30)])
-    last_name=StringField("Last Name",validators=[InputRequired(),Length(min= 2, max=30)])
-    email=StringField("Email",validators=[InputRequired(), Length(min=5, max=50)])
+    first_name = StringField(
+        "First Name", validators=[InputRequired(), Length(min=2, max=30)]
+    )
+    last_name = StringField(
+        "Last Name", validators=[InputRequired(), Length(min=2, max=30)]
+    )
+    email = StringField("Email", validators=[InputRequired(), Length(min=5, max=50)])
     password = PasswordField("Password", validators=[InputRequired()])
+
 
 class LoginForm(FlaskForm):
     """Login form."""
 
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[Length(min=6)])
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[Length(min=6)])
+
 
 class AllSelection(FlaskForm):
-    selection_list= SelectField('Select category to save:')
+    selection_list = SelectField("Select category to save:")
